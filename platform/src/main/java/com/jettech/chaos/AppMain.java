f09f5b881aa5653b2126f8b9b582211576a6284f
norlandsoft <11669556@qq.com>
2025-07-01 22:17:27 +0800

    注释

--- platform/src/main/java/com/jettech/chaos/AppMain.java
+++ platform/src/main/java/com/jettech/chaos/AppMain.java
@@ -3,6 +3,12 @@ package com.jettech.chaos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * AppMain
+ * 
+ * @author eric.chai
+ * @date 2025-07-01
+ */
 @SpringBootApplication
 public class AppMain {
     public static void main(String[] args) {
--- platform/src/main/java/com/jettech/chaos/controller/PlatformController.java
+++ platform/src/main/java/com/jettech/chaos/controller/PlatformController.java
@@ -6,12 +6,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * PlatformController
+ * 
+ * @author eric.chai
+ * @date 2025-07-01
+ */
 @Slf4j
 @RestController
 @RequestMapping("/api/platform")
 public class PlatformController {
   @GetMapping("/hello")
   public String hello() {
+    log.info("Hello, World!");
     return "Hello, World!";
   }
 }
